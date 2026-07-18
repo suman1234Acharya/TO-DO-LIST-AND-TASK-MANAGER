93a062ef389ca8f111c4f875d4697cf4d084b2f9
Suman Acharya <sumanacarya1588@gmail.com>
2026-07-18 11:14:38 +0530

    Implement logout functionality in logout.php

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Remove all session variables
+session_unset();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to Login Page
+header("Location: index.php");
+exit();
+?>
